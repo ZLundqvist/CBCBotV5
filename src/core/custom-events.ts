e0f737e5ed2b76fdddab34ed1eddb05704d1bbb4
ZLundqvist <Zack.lundqvist@hotmail.com>
2021-10-10 17:34:34 +0200

    Improve logging of voice state updates

--- src/core/custom-events.ts
+++ src/core/custom-events.ts
@@ -2,7 +2,7 @@ import getLogger from '@utils/logger';
 import { getVoiceUpdateType, VoiceUpdateTypes } from '@utils/voice';
 import Discord from 'discord.js';
 
-const log = getLogger('core');
+const log = getLogger('voice');
 
 export interface VoiceStateUpdateCustom {
     oldState: Discord.VoiceState;
@@ -11,16 +11,22 @@ export interface VoiceStateUpdateCustom {
 }
 
 export function attachCustomEvents(client: Discord.Client<true>): void {
-    attachVoiceUpdateType(client);
+    attachVoiceStateUpdateTypeListener(client);
 }
 
-function attachVoiceUpdateType(client: Discord.Client<true>): void {
+function attachVoiceStateUpdateTypeListener(client: Discord.Client<true>): void {
     client.on('voiceStateUpdate', (oldState: Discord.VoiceState, newState: Discord.VoiceState) => {
         const type = getVoiceUpdateType(oldState, newState);
 
         // Only log update if was not bot that moved to reduce log spam
         if(newState.client.user?.id !== newState.member?.user.id) {
-            log.debug(`user ${type}: ${newState.member?.displayName}`);
+            if(type === 'connect') {
+                log.debug(`User connect (user: ${newState.member?.displayName}, guild: ${newState.guild.name}, channel: ${newState.channel?.name})`);
+            } else if(type === 'disconnect') {
+                log.debug(`User disconnect (user: ${newState.member?.displayName}, guild: ${newState.guild.name}, channel: ${oldState.channel?.name})`);
+            } else if(type === 'transfer') {
+                log.debug(`User transfer (user: ${newState.member?.displayName}, guild: ${newState.guild.name}, old_channel: ${oldState.channel?.name}, new_channel: ${newState.channel?.name})`);
+            }
         }
 
         client.emit('voiceStateUpdateCustom' as any, { oldState, newState, type } as VoiceStateUpdateCustom);
