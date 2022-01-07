5dc094675cf64fd1a12108505dfa08c3667295af
ZLundqvist <Zack.lundqvist@hotmail.com>
2022-01-07 15:11:44 +0100

    Refactor: utils/logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,90 +7,93 @@ type TimeMeasurement = {
      */
     startTimestamp: number;
 
+    /**
+     * The {@link log4js.Level} to use when logging result at end of measurement
+     */
     level: log4js.Level;
 };
 
+/**
+ * A wrapper for the {@link log4js.Level} class that adds some extended functionality
+ */
 export class LoggerWrapper {
-    private readonly _logger: log4js.Logger;
-    private readonly _activeMeasurements: Map<string, TimeMeasurement>;
+    private readonly logger: log4js.Logger;
+    private readonly activeMeasurements: Map<string, TimeMeasurement> = new Map();
 
     constructor(name: string) {
-        this._logger = log4js.getLogger(name);
-        this._activeMeasurements = new Map();
+        this.logger = log4js.getLogger(name);
     }
 
     get name(): string {
-        return this._logger.category;
+        return this.logger.category;
     }
 
     set level(level: log4js.Level | string) {
         if(typeof level === 'string') {
-            this._logger.level = level;
+            this.logger.level = level;
         } else {
-            this._logger.level = level.levelStr;
+            this.logger.level = level.levelStr;
         }
     }
 
     get level(): string {
-        return this._logger.level;
+        return this.logger.level;
     }
 
     info(message: any, ...args: any[]): void {
-        this._logger.info(message, ...args);
+        this.logger.info(message, ...args);
     }
 
     warn(message: any, ...args: any[]): void {
-        this._logger.warn(message, ...args);
+        this.logger.warn(message, ...args);
     }
 
     error(message: any, ...args: any[]): void {
-        this._logger.error(message, ...args);
+        this.logger.error(message, ...args);
     }
 
     debug(message: any, ...args: any[]): void {
-        this._logger.debug(message, ...args);
+        this.logger.debug(message, ...args);
     }
 
     fatal(message: any, ...args: any[]): void {
-        this._logger.fatal(message, ...args);
+        this.logger.fatal(message, ...args);
     }
 
     trace(message: any, ...args: any[]): void {
-        this._logger.trace(message, ...args);
+        this.logger.trace(message, ...args);
     }
 
     /**
-     * Log if given level is enabled.
+     * Log using a dynamic {@link log4js.Level}.
      */
     log(level: log4js.Level, ...args: any[]): void {
-        this._logger.log(level, ...args);
+        this.logger.log(level, ...args);
     }
 
     time(key: string, level = log4js.levels.INFO): void {
-        const measurement = this._activeMeasurements.get(key);
-
-        if(measurement) {
-            this.warn(`Time is already being measured for key: ${key}`);
+        if(this.activeMeasurements.has(key)) {
+            this.warn(`Timer already exists for key: ${key}`);
             return;
         }
 
-        this._activeMeasurements.set(key, {
+        this.activeMeasurements.set(key, {
             startTimestamp: performance.now(),
             level: level
         });
     }
 
     timeEnd(key: string): void {
-        const measurement = this._activeMeasurements.get(key);
+        const measurement = this.activeMeasurements.get(key);
 
         if(!measurement) {
-            this.warn(`Time is not being measured for key: ${key}`);
+            this.warn(`Timer does not exist for key: ${key}`);
             return;
         }
 
         const elapsedMs = Math.ceil(performance.now() - measurement.startTimestamp);
         this.log(measurement.level, `${key}: ${elapsedMs}ms`);
-        this._activeMeasurements.delete(key);
+        this.activeMeasurements.delete(key);
     }
 }
 
