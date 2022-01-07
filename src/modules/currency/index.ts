c7459cab8229cdde20e57071b6f2f0e6af069aef
ZLundqvist <Zack.lundqvist@hotmail.com>
2022-01-07 15:11:54 +0100

    Refactor: modules/currency

--- src/modules/currency/index.ts
+++ src/modules/currency/index.ts
@@ -1,10 +1,10 @@
-import Discord, { EmbedField } from 'discord.js';
+import Discord from 'discord.js';
 import moment from 'moment';
 import { Colors, Images } from '../../constants';
 import { BotCore, Module } from "../../core";
 
 class CurrencyModule extends Module {
-    distributer?: NodeJS.Timeout;
+    distributer?: NodeJS.Timer;
 
     constructor() {
         super('currency');
@@ -26,39 +26,42 @@ class CurrencyModule extends Module {
     async getMemberEmbed(member: Discord.GuildMember): Promise<Discord.MessageEmbed> {
         const currency = await BotCore.database.getMember(member);
 
-        const embed = new Discord.MessageEmbed();
-        embed.setAuthor(member.displayName, Images.currencyLogo);
-        embed.setColor(Colors.gold);
-        embed.addFields([
-            {
-                name: 'Amount' + widen(15), // Repeat for formatting purposes
-                value: currency.currency + ' gold',
-                inline: true
-            }
-        ]);
-        embed.setFooter(`Tracked since: ${moment(currency.createdDate).fromNow()}`)
-
-        return embed;
+        return new Discord.MessageEmbed()
+            .setAuthor({
+                name: member.displayName,
+                iconURL: Images.currencyLogo
+            })
+            .setColor(Colors.gold)
+            .addFields([
+                {
+                    name: 'Amount', // Repeat for formatting purposes
+                    value: currency.currency + ' gold',
+                    inline: true
+                }
+            ])
+            .setFooter({
+                text: `Tracked since: ${moment(currency.createdDate).fromNow()}`
+            });
     }
 
     async getTopEmbed(guild: Discord.Guild, spots: number = 10): Promise<Discord.MessageEmbed> {
         const top = await BotCore.database.getMemberCurrencyTop(guild, spots);
 
-        const fields: Discord.EmbedField[] = [];
-        const embed = new Discord.MessageEmbed();
-        embed.setAuthor('Gold Top ' + spots + widen(6), Images.currencyLogo);
-        embed.setColor(Colors.gold);
+        const embed = new Discord.MessageEmbed()
+            .setAuthor({
+                name: `Currency Leaderboard`,
+                iconURL: Images.currencyLogo
+            })
+            .setColor(Colors.gold);
 
         top.forEach((currency, index) => {
             const member = guild.members.resolve(currency.id);
 
-            fields.push({
-                name: `#${index + 1} **${member ? member.displayName : 'UNKNOWN_USER'}**`,
-                value: `${currency.currency} gold`
-            } as EmbedField);
+            if(member) {
+                embed.addField(`#${index + 1} **${member.displayName}**`, `${currency.currency} gold`);
+            }
         });
 
-        embed.addFields(fields);
         return embed;
     }
 
@@ -67,27 +70,33 @@ class CurrencyModule extends Module {
         return guildDB.gpm;
     }
 
-    async setGPM(guild: Discord.Guild, newValue: number) {
+    async setGPM(guild: Discord.Guild, newValue: number): Promise<void> {
         const guildDB = await BotCore.database.getGuild(guild);
         guildDB.gpm = newValue;
         await guildDB.save();
     }
 
-    async distributeGold(client: Discord.Client<true>) {
-        for(const guild of client.guilds.cache.values()) {
+    private async distributeGold(client: Discord.Client<true>): Promise<void> {
+        let distributedAmount = 0;
+        let receivingMemberCount = 0;
+
+        const guilds = client.guilds.cache.values();
+        for(const guild of guilds) {
             try {
                 const guildSettings = await BotCore.database.getGuild(guild);
 
-                const voiceChannels = Array.from(guild.channels.cache.values())
-                    .filter((channel): channel is Discord.VoiceChannel => channel.isVoice() && channel.type === 'GUILD_VOICE') // Only take VoiceChannel
-                    .filter(vc => vc.guild.afkChannelId !== vc.id);  // Remove AFK channels
+                const voiceChannels = guild.channels.cache
+                    .filter((channel): channel is Discord.VoiceChannel => channel.type === 'GUILD_VOICE') // Remove non-VoiceChannels
+                    .filter((channel) => channel.id !== guild.afkChannelId); // Remove AFK channel
 
+                const connectedMembers = voiceChannels
+                    .flatMap(vc => vc.members)
+                    .filter(member => !member.user.bot); // Do not distribute gold to bots
 
-                // Gathers all connected members and flattens that array
-                const connectedMembers = voiceChannels.map(vc => Array.from(vc.members.values())).flat();
-
-                for(const member of connectedMembers) {
+                for(const member of connectedMembers.values()) {
                     await BotCore.database.addMemberCurrency(member, guildSettings.gpm);
+                    distributedAmount += guildSettings.gpm;
+                    receivingMemberCount += 1;
                 }
             } catch(error) {
                 if(error instanceof Error) {
@@ -95,19 +104,11 @@ class CurrencyModule extends Module {
                 } else {
                     this.log.error(error);
                 }
-
             }
         }
-        this.log.trace('Currency distributed');
-    }
-}
 
-/**
- * Adds x number of tabs and then a invisiable char to indent lines in embeds
- * @param n width
- */
-function widen(n: number) {
-    return '\t'.repeat(n) + '\u200C';
+        this.log.trace(`Currency distributed (${distributedAmount} gold distributed to ${receivingMemberCount} members)`);
+    }
 }
 
 export default new CurrencyModule();
